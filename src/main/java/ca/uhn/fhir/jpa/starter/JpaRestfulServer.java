64332d5eb0cac8d2279fbcedd3d1c303d73a10a7
BeatrizRCorreia <beatriz.correia.pt@gmail.com>
2023-07-31 10:15:44 +0100

    removing references to keycloak and multi tenancy

--- src/main/java/ca/uhn/fhir/jpa/starter/JpaRestfulServer.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/JpaRestfulServer.java
@@ -1,29 +1,17 @@
 package ca.uhn.fhir.jpa.starter;
 
-import java.util.Arrays;
-
 import javax.servlet.ServletException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Import;
-import org.springframework.web.cors.CorsConfiguration;
 
 import ca.uhn.fhir.jpa.model.config.PartitionSettings;
-import ca.uhn.fhir.jpa.starter.medunited.JWTTenantIdentificationStrategy;
-import ca.uhn.fhir.rest.server.interceptor.CorsInterceptor;
-import ca.uhn.fhir.rest.server.interceptor.partition.RequestTenantPartitionInterceptor;
 
 @Import(AppProperties.class)
 public class JpaRestfulServer extends BaseJpaRestfulServer {
 
   @Autowired
   AppProperties appProperties;
-
-  @Autowired
-  private PartitionSettings myPartitionSettings;
-  
-  @Autowired
-  private JWTTenantIdentificationStrategy jWTTenantIdentificationStrategy;
 
   private static final long serialVersionUID = 1L;
 
@@ -36,16 +24,6 @@ public class JpaRestfulServer extends BaseJpaRestfulServer {
     super.initialize();
 
     // Add your own customization here
-
-    // Enable partitioning
-    myPartitionSettings.setPartitioningEnabled(true);
-
-    // Set the tenant identification strategy
-    setTenantIdentificationStrategy(jWTTenantIdentificationStrategy);
-
-    // Use the tenant ID supplied by the tenant identification strategy
-    // to serve as the partitioning ID
-    registerInterceptor(new RequestTenantPartitionInterceptor());
 
   }
 
